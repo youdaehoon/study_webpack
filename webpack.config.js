fc69e0aed6a72f6392f77c97b8ab7bc36029d0e9
youdaehoon <107843412+youdaehoon@users.noreply.github.com>
2022-10-11 14:03:40 +0900

    plugin htmlwebpack 적용

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
-const path=require("path")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports={
-    mode:"development",
-    entry:{index:"./source/index.js",about:"./source/about.js"},
-    output:{
-        filename:"[name]_bundle.js",
-        path: path.resolve(__dirname,"public")
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
+module.exports = {
+  mode: "development",
+  entry: { index: "./source/index.js", about: "./source/about.js" },
+  output: {
+    filename: "[name]_bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-}
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./source/index.html",
+      filename:"./index.html",
+      chunks: ["index"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./source/about.html",
+      filename:"./about.html",
+      chunks: ["about"],
+    }),
+  ],
+};
